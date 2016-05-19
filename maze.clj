265043ac968003766e395f56e72aefa90e20a22a
achernyak <artem@ikayzo.com>
2016-05-18 21:15:17 -0500

    drawing maze

--- maze.clj
+++ maze.clj
@@ -15,3 +15,31 @@
           (recur (reduce disj walls (map set steps))
                  (reduce disj unvisited (keys steps))))
         walls))))
+
+(defn grid
+  [w h]
+  (set (concat
+        (for [i (range (dec w)) j (range h)] #{[i j] [(inc i) j]})
+        (for [i (range w) j (range (dec h))] #{[i j] [i (inc j)]}))))
+
+(defn draw
+  [w h maze]
+  (doto (javax.swing.JFrame. "Maze")
+    (.setContentPane
+     (doto (proxy [javax.swing.JPanel] []
+             (paintComponent [^java.awt.Graphics g]
+               (let [g (doto ^java.awt.Graphics2D (.create g)
+                         (.scale 10 10)
+                         (.translate 1.5 1.5)
+                         (.setStroke (java.awt.BasicStroke. 0.4)))]
+                 (.drawRect g -1 -1 w h)
+                 (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                   (let [[xc yc] (if (= xa xb)
+                                   [(dec xa) ya]
+                                   [xa (dec ya)])]
+                     (.drawLine g xa ya xc yc))))))
+       (.setPreferredSize (java.awt.Dimension.
+                           (* 10 (inc w)) (* 10 (inc h))))))
+    .pack
+    (.setVisible true)))
+(draw 40 40 (maze (grid 40 40)))
